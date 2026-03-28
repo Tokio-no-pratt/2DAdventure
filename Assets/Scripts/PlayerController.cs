6635eee94d49275415e4f4108d0643e9f2891685
Tokio <suelennascimento164@gmail.com>
2026-03-28 00:05:03 +0000

    transparencis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,28 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.InputSystem;
+
+
 public class PlayerController : MonoBehaviour
 {
-    float horizontal = 0.0f;
-    public InputAction LeftAction;
-       public InputAction MoveAction;
+  public InputAction MoveAction;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-   {
-      MoveAction.Enable();  
-      //QualitySettings.vSyncCount = 0;
-      //Application.targetFrameRate = 10;
-   }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-     
-
+  // Start is called before the first frame update
+  void Start()
+  {
+     MoveAction.Enable();   
+  }
+ 
+  // Update is called once per frame
+  void Update()
+  {
      Vector2 move = MoveAction.ReadValue<Vector2>();
      Debug.Log(move);
-     Vector2 position = (Vector2)transform.position + move * 5.1f * Time.deltaTime;
+      Vector2 position = (Vector2)transform.position + move * 5.0f * Time.deltaTime;
      transform.position = position;
-   
-    }
+  }
+
 
 }
-
