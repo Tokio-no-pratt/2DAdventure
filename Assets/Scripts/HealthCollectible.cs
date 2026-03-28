0393003acc6289e578993b837a516c6b2974ab9c
Tokio <suelennascimento164@gmail.com>
2026-03-28 14:34:38 +0000

    frutinha

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -1,16 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class HealthCollectible : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+void OnTriggerEnter2D(Collider2D other)
+{
+   PlayerController controller = other.GetComponent<PlayerController>();
+
+
+   if (controller != null && controller.health< controller.maxHealth)
+   {
+       controller.ChangeHealth(1);
+       Destroy(gameObject);
+   }
+}
 }
